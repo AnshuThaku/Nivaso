d1912115764a0857c96506307547598310a90e05
Anshu kumar <anshthakurcs236@gmail.com>
2026-03-16 09:04:53 +0530

    signup fix

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -5,29 +5,51 @@ const bcrypt = require("bcryptjs");
 const ExpressError = require("../utils/ExpressError");
 
 module.exports.signup = async (req, res) => {
-    const { email, password, username } = req.body;
+  const { email, password, username } = req.body;
+
+
 
     if(!email || !password || !username) {
+
         throw new ExpressError("Email, username and password are required", 400);
+
     }
+
     const userExists = await User.findOne({ email });
+
     if (userExists) {
+
         throw new ExpressError("Email already in use", 400);
+
     }
+
     const hashedPassword = await bcrypt.hash(password, 12);
+
     const user = new User({ email, username, password: hashedPassword });
+
     await user.save();
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-    res.cookie("token", token, );
+
+    // 🔥 Yahan options add karna zaroori hai
+    const cookieOptions = {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true,      // Kyunki Vercel/Render HTTPS use karte hain
+        sameSite: "none",  // Kyunki Frontend aur Backend alag domains par hain
+    };
+
+    res.cookie("token", token, cookieOptions);
+    
     await sendWelcomeEmail(email, username);
-res.json({
-    user: {
-        _id: user._id,
-        email: user.email,
-        username: user.username
-    },
-    token
-});
+    
+    res.json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            username: user.username
+        },
+        token
+    });
 };
 module.exports.login = async (req, res) => {
     const { email, password } = req.body;
