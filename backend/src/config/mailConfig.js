6d5b2932618ddace7b48048058740fa8fedf29b4
Anshu kumar <anshthakurcs236@gmail.com>
2026-03-16 14:19:07 +0530

    mailConfig fix

--- backend/src/config/mailConfig.js
+++ backend/src/config/mailConfig.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com", // 'service: gmail' ki jagah ye use karein
+  port: 465,              // SSL Port
+  secure: true,           // true for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
