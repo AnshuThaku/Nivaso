2dfc737ffa2444c6deda71f6a1da1adae255fbcb
Anshu thakur <163158412+AnshuThaku@users.noreply.github.com>
2026-03-22 01:53:13 +0530

    Configure DNS servers in server.js
    Set custom DNS servers for the application.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ if (process.env.NODE_ENV != "production") {
 const app = require("./src/app");
 const { connectDB } = require("./src/config/db");
 const ExpressError = require("./src/utils/ExpressError"); 
+const dns = require("dns");
+
+dns.setServers(["1.1.1.1","8.8.8.8"])
 
 const port = process.env.PORT || 8080;
 
